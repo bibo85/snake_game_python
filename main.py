d82977994657844135d64d9d0b2c6cd17d65f036
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-09-13 00:55:41 +0500

    Создание начального окна игры и обработка события при нажатии на крестик

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 
 import pygame
 
+WINDOW_SIZE = [400, 600]
+
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption('Змейка')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
