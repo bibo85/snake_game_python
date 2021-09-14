63119f8a6e4688203cb3c782b148e5c34415b5dd
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-09-15 01:12:41 +0500

    Динамическая разметка игрового поля. Добавил шапку для окна под картинку

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 
 import pygame
 
-SIZE_BLOCK = 20  # размер блоке
-COUNT_BLOCKS = 20  # количество блоков в ряду и колонках
-WINDOW_SIZE = [500, 600]  # размер начального экрана
-FRAME_COLOR = (0, 255, 204)  # цвет заливки окна
+SIZE_BLOCK = 20  # размер блока
+COUNT_BLOCKS = 20  # количество блоков в рядах и колонках
 MARGIN = 1  # отступ между блоками
+HEADER_MARGIN = 70  # отступ шапки
+WIDTH = SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * (COUNT_BLOCKS)
+HEIGHT = HEADER_MARGIN + 2 * SIZE_BLOCK + SIZE_BLOCK * COUNT_BLOCKS + MARGIN * (COUNT_BLOCKS)
+WINDOW_SIZE = [WIDTH, HEIGHT]  # размер начального экрана
 
 # цвета блоков поля
+FRAME_COLOR = (0, 255, 204)  # цвет заливки окна
 WHITE_COLOR = (255, 255, 255)
 BLUE_COLOR = (204, 255, 255)
+HEADER_COLOR = (0, 204, 153)  # цвет заливки шапки
 
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Змейка')  # title окна
@@ -24,15 +28,18 @@ while True:
 
     screen.fill(FRAME_COLOR)  # заливаем фон
 
-    # рисуем игровой блок на поле
+    # Заливаем шапку окна цветом
+    pygame.draw.rect(screen, HEADER_COLOR, [0, 0, WIDTH, HEADER_MARGIN])
+
+    # рисуем игровое поле
     for row in range(COUNT_BLOCKS):
         for column in range(COUNT_BLOCKS):
             if (row + column) % 2 == 0:
                 color = BLUE_COLOR
             else:
                 color = WHITE_COLOR
-            pygame.draw.rect(screen, color, [10 + column * SIZE_BLOCK + MARGIN * column,
-                                             20 + row * SIZE_BLOCK + MARGIN * row,
+            pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * column,
+                                             HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * row,
                                              SIZE_BLOCK,
                                              SIZE_BLOCK]
                              )
